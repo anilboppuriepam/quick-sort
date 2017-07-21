2e148afb2cec5b4831e42b3b819ee2f14f5b94c1
Anil Boppuri <Anil_Boppuri@epam.com>
2017-07-21 18:04:06 +0530

    first commit

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -2,5 +2,33 @@
 
 function quickSort(input) {
 
+          if(! (Array.isArray(input)))
+			  throw Error('input is not an array ');
+          if(input.length < 0)
+			 throw Error('the array has no elements');
+		  else if( input.length == 1)
+			 return input;
+   		  else if(input.length > 1){
+			  
+			  for(var i=0;i<input.length;i++){
+				  
+				  for(var j=0;j<input.length-1;j++){
+					  
+					    if(input[j] > input[j+1]){
+							
+							    var temp = input[j];
+								input[j] = input[j+1];
+								input[j+1]= temp;
+						}
+					  
+				  }
+			  }
+			  return input;
+		  }
+			  
+
+       
+	   
+		
 };
 module.exports = quickSort;
